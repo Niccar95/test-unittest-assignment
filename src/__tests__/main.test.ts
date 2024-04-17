3efed00bb946ade3047b60ab5a3783dcbb1db24f
Niccar95 <niccar.95@outlook.com>
2024-04-17 16:11:03 +0200

    setup mockup for createNewhtml

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,15 +1,55 @@
+import { createNewTodo } from "../ts/main";
 import { IAddResponse } from "../ts/models/IAddResponse";
+import { Todo } from "../ts/models/todo";
 import * as functions from "./../ts/functions";
+import * as htmlFunctions from "./../ts/htmlFunctions";
 
-describe("add todos", () => {
-  let AddTodo: jest.SpyInstance<IAddResponse>;
+describe("Testing individual functions", () => {
+  let mockAddTodo: jest.SpyInstance<IAddResponse>;
+  let mockCreateHTML: jest.SpyInstance<void>;
+  let mockDisplayError: jest.SpyInstance<void>;
+
   beforeEach(() => {
-    AddTodo = jest.spyOn(functions, "addTodo");
+    document.body.innerHTML = `<div id="app">
+    <form id="newTodoForm">
+      <div>
+        <input type="text" id="newTodoText" />
+        <button>Skapa</button>
+        <button type="button" id="clearTodos">Rensa lista</button>
+      </div>
+      <div id="error" class="error"></div>
+    </form>
+    <ul id="todos" class="todo"></ul>
+  </div>`;
+
+    mockAddTodo = jest.spyOn(functions, "addTodo");
+    mockCreateHTML = jest.spyOn(htmlFunctions, "createHtml");
+    mockDisplayError = jest.spyOn(htmlFunctions, "displayError");
   });
 
-  test("add the todos", () => {
-    AddTodo.mockImplementation(() => {
+  test("It should call the createHtml function", () => {
+    let todoText = "hello";
+    let todo: Todo[] = [];
+
+    mockAddTodo.mockImplementation(() => {
       return { success: true, error: "" };
     });
+
+    createNewTodo(todoText, todo);
+
+    expect(mockCreateHTML).toHaveBeenCalled();
+  });
+
+  test("It should call the displayError function", () => {
+    let todoText = "hello";
+    let todo: Todo[] = [];
+
+    mockAddTodo.mockImplementation(() => {
+      return { success: false, error: "bye" };
+    });
+
+    createNewTodo(todoText, todo);
+
+    expect(mockDisplayError).toHaveBeenCalled();
   });
 });
